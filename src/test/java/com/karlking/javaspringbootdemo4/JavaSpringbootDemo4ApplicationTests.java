02a9837a21cca6ab4df611541a2f5cf001afe0d1
Blackmamba2000 <knking85@gmail.com>
2020-12-28 10:06:53 -0600

    Added code to enable the swagger api documentation.

---
+++ src/test/java/com/karlking/javaspringbootdemo4/JavaSpringbootDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlking.javaspringbootdemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootDemo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
